02815901790f91a02df13e9cad92f84262ecc0e3
yiuriel <sebastian.murawczik@gmail.com>
2025-04-17 09:09:10 +0200

    add vite config

---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+// vite.config.ts
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "/dungeon-fighters/", // Replace with your repository name
+  // ...other config
+});
